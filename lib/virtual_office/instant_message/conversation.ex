aa787651437628699110bed8667ec8d356369433
Henry <henry@DESKTOP-937PL9E>
2021-05-10 00:10:37 +0100

    Conversation Persistance.

--- lib/virtual_office/instant_message/conversation.ex
+++ lib/virtual_office/instant_message/conversation.ex
@@ -1,9 +1,29 @@
 defmodule VirtualOffice.InstantMessage.Conversation do
-  @derive {Jason.Encoder, only: [:id, :messages, :user_ids]}
-  defstruct id: "", message_id: 1, messages: [], user_ids: []
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.Query, warn: false
+  alias VirtualOffice.Repo
 
+  alias VirtualOffice.Group.Association
+  alias VirtualOffice.Group.Member
   alias VirtualOffice.InstantMessage.Conversation, as: Conversation
   alias VirtualOffice.InstantMessage.Message, as: Message
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "conversations" do
+    field :individual, :boolean
+    belongs_to :user1, User, foreign_key: :individual_user_id1
+    belongs_to :user2, User, foreign_key: :individual_user_id2
+    field :message_count, :integer
+    has_many :messages, Message
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
 
   def new(conversation_id) do
     %Conversation{id: conversation_id}
@@ -26,7 +46,14 @@ defmodule VirtualOffice.InstantMessage.Conversation do
       true ->
         IO.puts("#{conversation.id}: Message Added.")
         new_message = Message.new(conversation.message_id, user_id, message_content)
-        {{:ok, new_message}, %Conversation{conversation | messages: [new_message | conversation.messages], message_id: conversation.message_id + 1}}
+
+        {{:ok, new_message},
+         %Conversation{
+           conversation
+           | messages: [new_message | conversation.messages],
+             message_id: conversation.message_id + 1
+         }}
+
       false ->
         IO.puts("#{conversation.id}: Invalid User.")
         {{:error, :invalid_user}, conversation}
@@ -38,6 +65,7 @@ defmodule VirtualOffice.InstantMessage.Conversation do
       true ->
         IO.puts("#{conversation.id}: User Already Exists.")
         {{:error, :user_exists}, conversation}
+
       false ->
         IO.puts("#{conversation.id}: User Added.")
         {:ok, %Conversation{conversation | user_ids: [new_user_id | conversation.user_ids]}}
