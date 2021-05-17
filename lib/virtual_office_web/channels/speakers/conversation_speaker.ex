ad06e92768fc7a06b6fd6a88905c6e0bb8898ce1
Henry <henry@DESKTOP-937PL9E>
2021-05-18 00:55:56 +0100

    Quick Update

--- lib/virtual_office_web/channels/speakers/conversation_speaker.ex
+++ lib/virtual_office_web/channels/speakers/conversation_speaker.ex
@@ -1,9 +1,13 @@
 defmodule VirtualOfficeWeb.ConversationSpeaker do
   alias VirtualOffice.Communication.Message
 
+  alias VirtualOfficeWeb.MessageView
+
   def speak({:message_new, message = %Message{}}, conversation_id) do
-    VirtualOfficeWeb.Endpoint.broadcast!("conversation:" <> conversation_id, "message_new", %{
-      data: message
-    })
+    VirtualOfficeWeb.Endpoint.broadcast!(
+      "conversation:" <> conversation_id,
+      "message_new",
+      Phoenix.View.render(MessageView, "get_message.json", message: message)
+    )
   end
 end
